a695f03aecf77c8099fcc2722f6c155a2a05c73c
Никита Филянин <famely.FSA@yandex.ru>
2022-04-08 12:33:57 +0300

    Добавьте файлы проекта.

---
+++ part4/part4.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+class Fraction {
+public:
+    Fraction(int _numeration, int _denumenator) {
+        if (_denumenator == 0) {
+            throw  std::runtime_error("Rintime error");
+        }
+    }
+private:
+    int numeration;
+    int denumerator;
+};
+
+
+int main()
+{
+    int first_number;
+    int second_number;
+
+    std::cout << "Enter first integer number\n";
+
+    try {
+
+        if (!(std::cin >> first_number)) {
+
+            throw(std::exception("Error! Wrong data!"));
+
+        }
+        if (!(std::cin >> second_number)) {
+
+            throw(std::exception("Error! Wrong data!"));
+
+        }
+
+        Fraction fraction = Fraction(first_number, second_number);
+    }
+    catch (std::exception ex) {
+
+        std::cout << ex.what();
+
+    }
+    catch (std::runtime_error runtimeError) {
+
+        std::cout << runtimeError.what();
+
+    }
+}
